8419bad2b696efaea2adc4d449cdfa39132e5959
Konstantin <123>
2025-10-29 23:46:06 +0900

    next

--- task.js
+++ task.js
@@ -15,3 +15,45 @@
 		console.log(loadindex)
 
     };
+
+let categoryesList = ["Architecture", "Art & Fashion", "Biography", "Business",
+	"Crafts & Hobbies", "Drama", "Fiction", "Food & Drink", "Health & Wellbeing", 
+	"History & Politics", "Humor", "Poetry", "Psychology", "Science", "Technology", 
+	"Travel & Maps"
+]
+
+
+
+
+
+function addInfoToHtml (object) {
+
+	const contentTextCity = document.querySelector(".content__text--city")
+	const contentTextArea = document.querySelector(".content__text--area")
+	const contentTextRepairTime = document.querySelector(".content__text--repair-time")
+	const contentTextRepairRcost = document.querySelector(".content__text--repair-rcost")
+	const contentContainerImg = document.querySelector(".content__container--img")
+
+	contentTextCity.innerHTML = `${object.city}<br>${object.district}`
+	contentTextArea.innerHTML = `${object["apartment area"]}`
+	contentTextRepairTime.innerHTML = `${object["repair time"]}`
+	contentTextRepairRcost.innerHTML = `${object["repair rcost"]}`
+	contentContainerImg.innerHTML = `<img src="${object['img sqr']}" alt="foto" class="content__img">` 
+
+}
+
+function addCategoriesToHtml(list) {
+	const contentCategories = document.querySelector(".content__categories")
+
+	const firstСategory = list[0]
+	const remainingListCategories = list.slice(1)
+
+	contentCategories.innerHTML += `<li data-id="${firstСategory}" class="content__item content__item--active">${firstСategory}</li>`
+	
+	for (cat of remainingListCategories) {
+		contentCategories.innerHTML += `<li data-id="${cat}" class="content__item">${cat}</li>`
+	}
+
+}
+
+addCategoriesToHtml(categoryesList)
